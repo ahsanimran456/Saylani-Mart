39f1f1c4289c805cf7e42b57e6be9c5a57de674e
unknown <ahxanimran456@gmail.com>
2023-02-15 01:03:17 +0500

    login screen ..........

--- SaylaniStore/Screens/Login/Login.js
+++ SaylaniStore/Screens/Login/Login.js
@@ -6,28 +6,140 @@ import {
     Text,
     useColorScheme,
     View,
-    setNativeProps,
+    TextInput, Dimensions
+
 } from 'react-native';
+
 import { useEffect, useRef } from 'react';
 import Splash from '../Splash/Splash';
+const { width, height } = Dimensions.get('window')
+
 function Login() {
-    const splashscreen = useRef();
+    const myRef = useRef();
     useEffect(() => {
         setTimeout(() => {
-            console.log(splashscreen.current.setNativeProps({
+            myRef.current.setNativeProps({
                 style: {
-                    display:"none"
+                    display: "none"
                 }
-            }))
+            })
+        }, 2000)
 
-        }, 2000);
     }, []);
     return (
         <SafeAreaView style={{ backgroundColor: "red", flex: 1 }}>
-            <Splash ref={splashscreen} />
+            <View ref={myRef} style={{ flex: 1 }}>
+                <Splash />
+            </View>
+            <View style={styles.main_login}>
+
+                <View style={styles.Header}>
+                    <View>
+                        <Text style={{ textAlign: "center", fontSize: 35, fontWeight: 800, color: "#61B846" }}>
+                            SAYLANI WELFARE
+                        </Text>
+                    </View>
+                    <View>
+                        <Text style={{ textAlign: "center", fontSize: 20, fontWeight: 600, color: "#024F9D" }}>
+                            ONLINE DISCOUNT STORE
+                        </Text>
+                    </View>
+                </View>
+
+                <View style={styles.login_body}>
+
+                    <View style={styles.parentinputs}>
+                        <View style={styles.parentinputs}>
+                            <TextInput
+                                style={styles.inputfields}
+                                placeholder="Full Name"
+
+                            />
+                        </View>
+                        <View>
+
+                        </View>
+                    </View>
+                    <View style={styles.parentinputs}>
+                        <View style={styles.parentinputs}>
+                            <TextInput
+                                style={styles.inputfields}
+                                placeholder="Contact"
+
+                            />
+                        </View>
+                        <View>
+
+                        </View>
+                    </View>
+                    <View style={styles.parentinputs}>
+                        <View style={styles.parentinputs}>
+                            <TextInput
+                                style={styles.inputfields}
+                                placeholder="Email"
+
+                            />
+                        </View>
+                        <View>
+
+                        </View>
+                    </View>
+                    <View style={styles.parentinputs}>
+                        <View style={styles.parentinputs}>
+                            <TextInput
+                                style={styles.inputfields}
+                                placeholder="Password"
+                                secureTextEntry={true}
+                            />
+                        </View>
+                        <View>
+
+                        </View>
+                    </View>
+
+                </View>
+            </View>
+
+
         </SafeAreaView>
 
     );
 }
+
+const styles = StyleSheet.create({
+    main_login: {
+        flex: 1,
+        display: "flex",
+
+        // justifyContent: "center",
+        // alignContent: "center",
+    },
+    Header: {
+        marginTop: 30,
+        // flex: 2
+    },
+    login_body: {
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        paddingVertical: 50
+    },
+    parentinputs: {
+        width: width,
+        display: "flex",
+        justifyContent: "center",
+        textAlign: "center",
+        alignItems: "center",
+        marginTop: 10,
+
+    },
+    inputfields: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#D4D3D3",
+        width: "70%",
+        paddingVertical: 2
+    }
+
+});
 
 export default Login;
