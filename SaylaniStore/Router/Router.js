32ae32e98decef110d9650dbb5f26ad2ebf846bf
unknown <ahxanimran456@gmail.com>
2023-02-20 00:57:06 +0500

    folder updation

--- SaylaniStore/Router/Router.js
+++ SaylaniStore/Router/Router.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Splash from '../Screens/Splash/Splash';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Splash from '../Screens/Splash/Splash';
 import Login from '../Screens/Login/Login';
-import User from '../Screens/User/User';
+import Drawernavigation from '../Drawer/DrawerScreens';
 import Admin from '../Screens/Admin/Admin';
 
 function Router() {
     const Stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Splash" component={Login} options={{ headerShown: false }} />
-                <Stack.Screen name="userhome" component={User} options={{ headerShown: false }} />
-                <Stack.Screen name="adminHome" component={Admin} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+                    {/* <Stack.Screen name="Drawernavigation" component={Drawernavigation} options={{ headerShown: false }} />
+                    <Stack.Screen name="adminHome" component={Admin} options={{ headerShown: false }} /> */}
+                </Stack.Navigator>
+            </NavigationContainer>
+
+            {/* <NavigationContainer>
+                <Tab.Navigator screenOptions={{ headerShown: false }}>
+                    <Tab.Screen name="adminHome" component={User} />
+                    <Tab.Screen name="Settings" component={SettingsStackScreen} />
+                </Tab.Navigator>
+            </NavigationContainer> */}
+        </>
     );
 }
 
