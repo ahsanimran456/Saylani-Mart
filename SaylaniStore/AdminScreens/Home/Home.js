70b5f7be10db758b524d27cc54497eb4aad4a4ae
unknown <ahxanimran456@gmail.com>
2023-02-24 01:15:27 +0500

    admin header

--- SaylaniStore/AdminScreens/Home/Home.js
+++ SaylaniStore/AdminScreens/Home/Home.js
@@ -16,11 +16,19 @@ function Home() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <ScrollView>
-                <View style={styles.headerparent }>
-                    <View>
+                <View style={styles.headerparent}>
+                    <View style={styles.headerinner}>
                         <AntDesign name="left" size={30} style={{ color: "#BFBCBC" }} />
+                        <View style={{ width: 50, height: 50, borderColor: "red", borderWidth: 2, borderRadius: 100,marginRight:5 }}>
+                            <Image source={require("../../Assets/images/item6.jpg")} style={{ borderRadius: 100, height: "100%", width: "100%", objectFit: "cover" }} />
+                        </View>
                         <View>
-                            <Image source={require("../../Assets/images/item6.jpg")}/>
+                            <Text style={styles.headertext1}>
+                                Mr. Ahsan
+                            </Text>
+                            <Text style={styles.headertext2}>
+                                Admin
+                            </Text>
                         </View>
                     </View>
                     <View></View>
@@ -31,11 +39,26 @@ function Home() {
     );
 }
 const styles = StyleSheet.create({
-    headerparent:{
-        paddingHorizontal:10,
-        paddingVertical:10
+    headerparent: {
+        paddingHorizontal: 10,
+        paddingVertical: 10
 
+    },
+    headerinner: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    headertext1: {
+        color: "#024F9D",
+        fontSize:20
+    },
+    headertext2: {
+        color: "#61B846",
+        fontSize:17
     }
+
+
 })
 
 export default Home;
